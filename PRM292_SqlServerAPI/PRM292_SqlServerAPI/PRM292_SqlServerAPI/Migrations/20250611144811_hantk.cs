31ac919ad22ad757eef02abc2d0df1708378cd35
HaNTK <khanhhanguyen1123@gmail.com>
2025-06-11 23:33:39 +0700

    update-final

---
+++ PRM292_SqlServerAPI/PRM292_SqlServerAPI/PRM292_SqlServerAPI/Migrations/20250611144811_hantk.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PRM292_SqlServerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class hantk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
